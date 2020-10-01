0c787bc9402555675177f171275cc0c845efb8ab
Willian Praciano <willian.s.praciano@gmail.com>
2020-10-01 09:38:23 -0300

    Authentication: Login with Github

--- 4 - Authentication/js/github.js
+++ 4 - Authentication/js/github.js
@@ -1,3 +1,11 @@
 function loginGithub() {
+  //Cria uma nova instancia do provedor de login do facebook
+  var provider = new firebase.auth.GithubAuthProvider();
 
+  firebase.auth().signInWithPopup(provider).then(resposta => {
+    console.log('Usuário: ', resposta.user);
+    console.log('Token: ', resposta.credential.accessToken);
+  }).catch(erro => {
+    console.log('Erro login com Google: ', erro);
+  });
 }
