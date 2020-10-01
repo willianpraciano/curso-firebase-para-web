dcdcebbc85ead8b51fdeacb67903ce552b947898
Willian Praciano <willian.s.praciano@gmail.com>
2020-10-01 10:10:46 -0300

    Authentication: Using FirebaseUI

--- 4 - Authentication/js/app.js
+++ 4 - Authentication/js/app.js
@@ -8,3 +8,32 @@ function logout() {
   });
 
 }
+
+
+/**
+ * Listener de dom ready
+ */
+document.addEventListener("DOMContentLoaded", function () {
+  //nova instancia do firebaseui
+  var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+  // Configurações do firebaseui
+  var config = {
+    callbacks : {
+      signInSuccessWithAuthResult: function(authResult){
+        console.log('authResult', authResult);
+        return false;
+      }
+    },
+    signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID
+    ],
+    signInFlow: 'popup'
+  };
+
+  // Inicializa o firebaseui
+  ui.start('#firebaseui-auth', config); // Só é executado depois de a DOM estar carregada
+});
