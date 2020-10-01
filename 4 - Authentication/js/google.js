535fc2cc64d428392017f6cf7d1f2a330b95b658
Willian Praciano <willian.s.praciano@gmail.com>
2020-10-01 09:19:06 -0300

    Authentication: Login with Google

--- 4 - Authentication/js/google.js
+++ 4 - Authentication/js/google.js
@@ -1,3 +1,11 @@
 function loginGoogle() {
+    //Cria uma nova instancia do provedor de login do facebook
+    var provider = new firebase.auth.GoogleAuthProvider();
 
+    firebase.auth().signInWithPopup(provider).then(resposta => {
+      console.log('Usuário: ', resposta.user);
+      console.log('Token: ', resposta.credential.accessToken);
+    }).catch(erro => {
+      console.log('Erro login com Google: ', erro);
+    });
 }
