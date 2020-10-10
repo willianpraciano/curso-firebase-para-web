0b0e8e57040960296452fd48231063f1d7f14bbe
Willian Praciano <willian.s.praciano@gmail.com>
2020-10-10 09:59:05 -0300

    Cloud Storage: Upload File

--- 3 - Storage/app.js
+++ 3 - Storage/app.js
@@ -1,14 +1,34 @@
-/**
+  /**
  * Variáveis com referencias dos inputs
  */
 var fileInput = document.getElementById('file-input');
 var stringInput = document.getElementById('string-input');
 
 /**
+ * Referencia para o storage do firebase criando uma pasta com o nome de arquivos; 
+ */
+var ref = firebase.storage().ref('arquivos');
+
+/**
  * Metodo que observa mudanças no input de arquivo
  */
 fileInput.onchange = function (event) {
-    
+    var arquivo = event.target.files[0]; //Força para que só seja selecionado 1 arquivo, o da posição [0]
+
+    /**
+     * .child(nome) - Acessar o caminho para inserir o arquivo.
+     * .put(arquivo) - insere o arquivo
+     */
+    ref.child('arquivo').put(arquivo).then(snapshot => {
+      console.log('snapshot', snapshot);
+
+      /**
+       * .getdownloadURL() - Retorna a url para baixar/mostrar o arquivo enviado
+       */
+      ref.child('arquivo').getDownloadURL().then(url => {
+        console.log('String para download: ', url)
+      });
+    });
 }
 
 /**
