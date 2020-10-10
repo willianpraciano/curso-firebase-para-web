5f41fea92156ab8e4a3a1f903dd35b70b7616bfb
Willian Praciano <willian.s.praciano@gmail.com>
2020-10-10 10:53:28 -0300

    Cloud Storage: Upload as String base64

--- 3 - Storage/app.js
+++ 3 - Storage/app.js
@@ -26,7 +26,7 @@ fileInput.onchange = function (event) {
        * .getdownloadURL() - Retorna a url para baixar/mostrar o arquivo enviado
        */
       ref.child('arquivo').getDownloadURL().then(url => {
-        console.log('String para download: ', url)
+        console.log('String para download: ', url);
       });
     });
 }
@@ -35,5 +35,32 @@ fileInput.onchange = function (event) {
  * Metodo que observa mudanças no input de string
  */
 stringInput.onchange = function (event) {
+  var arquivo = event.target.files[0];
+
+  const reader = new FileReader();
+  reader.readAsDataURL(arquivo);
+  reader.onload = function() {
+    console.log(reader.result); //retorna uma string que começa com 'base64,afaffdfsdfdf'
+    
+    /**
+     * Para pasar a string para o firebase é necessário remover o 'base64,',
+     * então usa-se o split, que dividde a string em duas, 
+     * sedo a posição [0] é a parte 'base64'
+     * então pega-se a posição [1]
+     */
+    const base64 = reader.result.split('base64,')[1];
+
+    /**
+     * .putString(string, formato, metadados) - Salva uma string no firebase, onde é possível
+     * colocar um formato de imagem para que automaticamente converta para um png 
+     */
+    ref.child('images').putString(base64, 'base64', {contentType: 'image/png'}).then(snapshot => {
+      console.log('snapshot', snapshot);
+
+      ref.child('images').getDownloadURL().then(url => {
+        console.log('string para download', url);
+      });
+    });
+  }
 
 }
