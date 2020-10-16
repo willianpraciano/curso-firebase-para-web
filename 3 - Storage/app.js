45a02dfd4624a42825732c5314f43e04a421c8b5
Willian Praciano <willian.s.praciano@gmail.com>
2020-10-16 07:55:55 -0300

    Cloud Storage: Adding metadata and managing file upload

--- 3 - Storage/app.js
+++ 3 - Storage/app.js
@@ -8,27 +8,69 @@ var stringInput = document.getElementById('string-input');
  * Referencia para o storage do firebase criando uma pasta com o nome de arquivos; 
  */
 var ref = firebase.storage().ref('arquivos');
+var tarefaDeUpload;
 
 /**
  * Metodo que observa mudanças no input de arquivo
  */
 fileInput.onchange = function (event) {
     var arquivo = event.target.files[0]; //Força para que só seja selecionado 1 arquivo, o da posição [0]
-
+    var uid = firebase.database().ref().push().key; //Esse metodo faz com que ele crie uma chave para você sem inserir nada no realtime
     /**
      * .child(nome) - Acessar o caminho para inserir o arquivo.
      * .put(arquivo) - insere o arquivo
      */
-    ref.child('arquivo').put(arquivo).then(snapshot => {
+    /*
+    ref.child(uid).put(arquivo, {
+      customMetadata: {
+        nome: 'Curriculo'
+      }
+    }).then(snapshot => {
       console.log('snapshot', snapshot);
 
-      /**
-       * .getdownloadURL() - Retorna a url para baixar/mostrar o arquivo enviado
-       */
-      ref.child('arquivo').getDownloadURL().then(url => {
+      // .getdownloadURL() - Retorna a url para baixar/mostrar o arquivo enviado
+      ref.child(uid).getDownloadURL().then(url => {
         console.log('String para download: ', url);
       });
+
+      ref.child(uid).getMetadata().then(metadata => {
+        console.log('metadados: ',metadata);
+      });
     });
+    */
+    
+    //METADADOS
+    //Inserindo metadados
+    /*
+    ref.child('arquivo').put(arquivo, {
+      customMetadata: {
+        nome: 'Curriculo',
+        descricao: 'Curriculo atualizado',
+      }
+    });
+    */
+
+    /**
+     * Buscando metadados
+     * getMetadata: retorna os metadados do arquivo inserido
+     */
+    /*
+    ref.child('arquivo').getMetadata().then(metadata => {
+      console.log(metadata);
+    });
+    */
+
+    //Atribui a tarefa de upload à variável e executa essa tarefa ao executar o .put()
+    tarefaDeUpload = ref.child(uid).put(arquivo);
+
+    tarefaDeUpload.then(snapshot => {
+      console.log('Snapshot: ', snapshot);
+    }).catch(error => {
+      //Pega o erro, que no caso é gerado pelo cancelamento da tarefa
+      console.log('Error: ', error);
+    });
+
+
 }
 
 /**
@@ -63,4 +105,22 @@ stringInput.onchange = function (event) {
     });
   }
 
+}
+
+//pausa a tarefa de upload  
+pausar = function() {
+  tarefaDeUpload.pause();
+  console.log('Pausou tarefa');
+}
+
+//continua a tarefa de upload  
+continuar = function() {
+  tarefaDeUpload.resume();
+  console.log('Continuou tarefa');
+}
+
+//cancela a tarefa de upload  
+cancelar = function() {
+  tarefaDeUpload.cancel();
+  console.log('Cancelou tarefa');
 }
